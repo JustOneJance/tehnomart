7fd939d49f8a1b033c28e9709dafed0a512c038b
JustOneJance <justonejance@gmail.com>
2018-06-12 21:46:04 +0300

    add src directory and change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
+"use strict";
+
+
 var gulp = require("gulp");
 var server = require("browser-sync").create();
 var sass = require("gulp-sass");
+var cssmin = require('gulp-minify-css');
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
+var prefixer = require("autoprefixer");
+var imagemin = require('gulp-imagemin');
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+var pngquant = require('imagemin-pngquant');
+var watch = require('gulp-watch');
+var rimraf = require('rimraf');
 
-gulp.task("style", function () {
+/**gulp.task("style", function () {
     gulp.src("sass/style.scss")
         .pipe(plumber())
         .pipe(sass())
@@ -25,7 +35,122 @@ gulp.task("serve", ["style"], function() {
         server: "."
     });
 
-    gulp.watch("sass/**/*.scss", ["style"]);
+ //   gulp.watch("sass/** /*.scss", ["style"]);
     gulp.watch("*.html")
         .on("change", server.reload);
+}); */
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/sass/style.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_allay"
+};
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html)
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
 });
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js)
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task("style:build", function () {
+    gulp.src("src/sass/style.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+
+            prefixer({browsers: [
+                    "last 2 versions"
+                ]})
+
+        ]))
+        .pipe(gulp.dest("build/css"))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
+
